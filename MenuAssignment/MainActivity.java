efc60e0ba1080a754aa88ec4a820477a63db8693
Varad Mhetar <155555913+VMHETAR@users.noreply.github.com>
2026-04-10 18:23:37 +0530

    Create MainActivity.java

---
+++ MenuAssignment/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.menuassignment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.menu_settings) {
+            startActivity(new Intent(this, ContextMenuActivity.class));
+            return true;
+        }
+
+        if (item.getItemId() == R.id.menu_about) {
+            startActivity(new Intent(this, PopupMenuActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
